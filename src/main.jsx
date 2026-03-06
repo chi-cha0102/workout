e887ea4723a3fa8622336c1390b155191f38cbfe
chi-cha0102 <chie.garnet2nd@gmail.com>
2026-03-06 22:55:36 +0900

    Add main.jsx to render GymRecordApp component

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GymRecordApp from "./GymRecordApp";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GymRecordApp />
+  </React.StrictMode>
+);
